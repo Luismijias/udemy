30cf674e6303ebdfaf49df969d387417c40f0348
Luismijias <luismijias@gmail.com>
2023-04-03 18:06:39 -0300

    automatic script commit.

---
+++ cod3r/Fundamentos/comentario.js
@@ -0,0 +1,20 @@
+//Comentário de uma única linha.
+
+console.log('linha 1')
+
+/* Comentário de 
+multiplas linhas */
+
+console.log ('linha 2')
+
+/*
+*Comentário de multiplas linhas
+*porém é desnecessário
+*tera asteriscos em todas 
+*as linhas
+*/
+
+
+
+
+
