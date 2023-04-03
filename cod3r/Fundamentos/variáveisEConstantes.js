46dc3876953a9ae21963ce0f51fdf869eced075e
Luismijias <luismijias@gmail.com>
2023-04-03 18:18:00 -0300

    automatic script commit.

--- cod3r/Fundamentos/variáveisEConstantes.js
+++ cod3r/Fundamentos/variáveisEConstantes.js
@@ -1,2 +1,18 @@
 var a = 3 // a recebe 3 . O igual não é comparador é recebe.
 
+let b = 4 // b recebe o valor 4.Let é mais moderno então é bom trabalhar com let.
+
+var a = 30 // var pode ser declarado novamente.
+ b = 40  //let não aceita redeclarar a variável.
+
+console.log(a,b)
+
+a=300
+b=400
+
+console.log(a,b)
+const c = 5
+
+// c = 50
+
+console.log(c)
