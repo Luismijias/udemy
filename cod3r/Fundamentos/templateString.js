f5af013e3b7c7e4142981459620bf04a0094ef9c
Luismijias <luismijias@gmail.com>
2023-04-06 16:55:53 -0300

    automatic script commit.

---
+++ cod3r/Fundamentos/templateString.js
@@ -0,0 +1,19 @@
+const nome = 'Rebeca'
+const concatenação = 'Olá ' + nome + '!'
+const template =`Olá ${nome}!`
+
+console.log("Concatenação padrão:",concatenação)
+console.log("Template String:",template)
+
+//expessões
+
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+
+console.log(`Ei... ${up('cuidado')}!`)
+
+
+
+
+
