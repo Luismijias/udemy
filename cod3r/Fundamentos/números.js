69ca03a79f964b98608e364c372ecdae1729864d
Luismijias <luismijias@gmail.com>
2023-04-06 15:17:22 -0300

    automatic script commit.

--- cod3r/Fundamentos/números.js
+++ cod3r/Fundamentos/números.js
@@ -3,11 +3,11 @@ const peso2 = Number('2.0')
 const peso3 = 1.1
 const peso4 = Number('2.2')
 
-console.log(peso1,peso2)
-console.log(Number.isInteger(peso1))// isInteger verifica se o número é inteiro.
-console.log(Number.isInteger(peso2))
-console.log(Number.isInteger(peso3))
-console.log(Number.isInteger(peso4))
+console.log("peso1 e peso2 =",peso1,peso2)
+console.log("peso1 é inteiro? =",Number.isInteger(peso1))// isInteger verifica se o número é inteiro.
+console.log("peso2 é inteiro? =",Number.isInteger(peso2))
+console.log("peso3 é inteiro? =",Number.isInteger(peso3))
+console.log("peso4 é inteiro? =",Number.isInteger(peso4))
 
 const avaliação1 = 9.871
 const avaliação2 = 6.871
@@ -15,4 +15,8 @@ const avaliação2 = 6.871
 const total = avaliação1 * peso1 + avaliação2 * peso2
 const media = total / (peso1 + peso2)
 
-console.log(media.toFixed(2)) //arredonda para apenas 2 casas decimais
+console.log("avaliação1 =",avaliação1)
+console.log("avaliação2 =",avaliação2)
+console.log("media das avaliações =",media.toFixed(2)) //arredonda para apenas 2 casas decimais
+console.log("valor demédia como string =",media.toString())
+console.log("valor demédia como string em valor binário =",media.toString(2))//converte o valor ponto flutuante em valor binário.
