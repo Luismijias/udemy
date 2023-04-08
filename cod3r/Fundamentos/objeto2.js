4d23424dd30fb8d61cf3d90b91c8cff0125e2fbf
Luismijias <luismijias@gmail.com>
2023-04-08 20:27:39 -0300

    automatic script commit.

--- cod3r/Fundamentos/objeto2.js
+++ cod3r/Fundamentos/objeto2.js
@@ -9,3 +9,29 @@ class Produto {} // ES 2015 (ES6)  neste caso é uma funçao
 console.log(typeof Produto)
 console.log(typeof new Produto()) //neste caso foi chamado objeto com parênteses
 
+
+
+
+//treinando
+function factoryFunctionCelular (marcaCelular,tamanhoTela,capacidadedeBateria){
+    return{
+        marcaCelular,
+        tamanhoTela,
+        capacidadedeBateria,
+        ligar(){
+            console.log('chamando...')
+        }
+    }
+}
+
+console.log(typeof 'ExemplofactoryFunctionCelular -->',factoryFunctionCelular('xiaomi',5.7,2800))
+function ContructionFunctionCelular(marcaCelular,tamanhoTela,capacidadedeBateria){
+    this.marcaCelular = marcaCelular
+    this.tamanhoTela = tamanhoTela
+    this.capacidadedeBateria = capacidadedeBateria
+    this.ligar = function Chamando_trimmmm_trimmmm() {
+        console.log();
+      }
+}
+const celular = new ContructionFunctionCelular ('ausus',5.5,2500)
+console.log(typeof 'Exemplo de constructor Function OBJETO --->',celular )
