d8155dd8111a72b49feaaf70c9b6d4e183098024
Luismijias <luismijias@gmail.com>
2023-04-08 19:39:21 -0300

    automatic script commit.

---
+++ cod3r/Fundamentos/objeto2.js
@@ -0,0 +1,11 @@
+console.log(typeof Object)
+console.log(typeof  new Object)//o objeto pode estar sem parênteses
+
+const Client = function(){}
+console.log(typeof Client)
+console.log(typeof new Client)
+
+class Produto {} // ES 2015 (ES6)  neste caso é uma funçao
+console.log(typeof Produto)
+console.log(typeof new Produto()) //neste caso foi chamado objeto com parênteses
+
