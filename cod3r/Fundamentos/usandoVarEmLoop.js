f9c2c82db0387fcf00bdd17cecbfd08a527120dc
Luismijias <luismijias@gmail.com>
2023-04-08 11:16:28 -0300

    automatic script commit.

---
+++ cod3r/Fundamentos/usandoVarEmLoop.js
@@ -0,0 +1,9 @@
+for (var i = -3; i < 6; i++){
+    console.log('comparando se é v ou f para igual à 6 :',i)
+}
+console.log('i =',i)
+
+for (var j = 10; j < 8; j++){
+    console.log('no caso o valor 10 é falso para menor que 8',j)
+}
+console.log('j =',j)
