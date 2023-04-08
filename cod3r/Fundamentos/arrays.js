fceac8e3a6e47b75511a109f77ac71ac2751f67e
Luismijias <luismijias@gmail.com>
2023-04-08 17:24:04 -0300

    automatic script commit.

--- cod3r/Fundamentos/arrays.js
+++ cod3r/Fundamentos/arrays.js
@@ -1,23 +1,26 @@
-const valores = [7.7, 8.9 ,6.3, 9.2]
-console.log(valores[0],valores[3])
-console.log(valores[4])// undefined porque não exite esse elemento.
+const valores = [7.7, 8.9, 6.3, 9.2];
+console.log('Elementos na posição 0 e 3 são:', valores[0], valores[3]);//mostra os elementos na posição 0 e 3
+console.log(valores[4]); // undefined porque não exite esse elemento.
 
-valores[4] = 10
+valores[4] = 10;
 
-console.log(valores[4])// agora recebeu 10 então aparece.
-console.log(valores)
-console.log(valores.length)// mostra o tamanho do array
+console.log(valores[4]); // agora recebeu 10 então aparece.
+console.log(valores);
+console.log(valores.length); // mostra o tamanho do array
 
-valores.push({id: 3}, false, null, 'teste')
-console.log(valores)
-
-console.log(valores.pop())//remove o ultimo valor do array
-console.log(valores.shift())//remove o primeiro elemento do array
+valores.push({ id: 3 }, false, null, "teste"); //O método push adiciona valores a um array.
 console.log(valores)
 
-delete valores[1] //deleta o indice selecionado
-console.log(valores)
+valores.push(727);
+console.log("array = com o 727 no final", valores);
 
-console.log(typeof valores)
+console.log(valores.pop()); //remove o ultimo valor do array
+console.log(valores);
 
+console.log(valores.shift()); //remove o primeiro elemento do array
+console.log(valores);
 
+delete valores[1]; //deleta o indice selecionado
+console.log(valores);
+
+console.log(typeof valores);
---
+++ cod3r/Fundamentos/usandoVarEmLoop2.js
@@ -0,0 +1,11 @@
+const funcs = [];
+
+for (var i = 0; i < 10; i++) {
+  funcs.push(function () {
+    console.log(i);
+  });
+}
+funcs[2]()
+funcs[8]() /** No caso a função "var" não tem escopo de 
+bloco os valores serão 10.Problema histórico do js um dos motivos
+de ter se criado o let**/
