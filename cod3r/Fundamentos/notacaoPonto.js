0af10f9083ad0cd037ea6994fad172f6b2627788
Luismijias <luismijias@gmail.com>
2023-04-11 20:55:39 -0300

    automatic script commit.

--- cod3r/Fundamentos/notacaoPonto.js
+++ cod3r/Fundamentos/notacaoPonto.js
@@ -1,20 +1,23 @@
-console.log(Math.ceil(6.1)) //arredonda para cima
-console.log(Math.floor(6.995)) //arredonda para baixo
-console.error("Atençao!!!")
+console.log("Arredonda valor para cima==>",Math.ceil(6.1))
+console.log("arredonda para baixo==>",Math.floor(6.995)) 
 
 const obj1 = {}
-obj1.nome = "bola"
-const obj2 = new Obj("Cadeira")
-const obj3 = new Obj("mesa")
+obj1.nome = "bola3333"
 // obj1['nome'] = 'bola2'//sobrescreve o atributo do objeto
+console.log("objeto 1 sem alteração ==> ", obj1)
+
+
 
 function Obj(nome) {
   this.nome = nome
+
   this.exec = function () {
     console.log("Exec...")
   }
   console.log("Objeto dentro da função =", nome)
 }
+const obj2 = new Obj("Cadeira")
+const obj3 = new Obj("mesa")
 console.log(obj1.nome)
 console.log(obj2.nome)
 console.log(obj3.nome)
