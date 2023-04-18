267753eedf5b6dd164e63781e9436be6777d98ea
Luismijias <luismijias@gmail.com>
2023-04-18 18:26:32 -0300

    automatic script commit.

--- cod3r/controle/ifElseIf.js
+++ cod3r/controle/ifElseIf.js
@@ -1,22 +1,20 @@
-
-
-Number.prototype.entre = function (inicio, fim){
-    return this >= inicio && this <=fim
+Number.prototype.entre = function (inicio, fim) {
+  return this >= inicio && this <= fim
 }
 
-const imprimirResultado = function (nota){
-    if (nota.entre(9, 10)) {
-        console.log('Quador de Honra')
-    } else if (nota.entre(7, 8.99)){
-        console.log('Aprovado')
-    } else if (nota.entre(4, 6.99)){
-        console.log('recuperação')
-    } else if (nota.entre(0, 3.99)){
-        console.log('Reprovado')
-    } else {
-        console.log('Nota inválida')
-    }        
-    console.log('Fim')
+const imprimirResultado = function (nota) {
+  if (nota.entre(9, 10)) {
+    console.log("Quador de Honra")
+  } else if (nota.entre(7, 8.99)) {
+    console.log("Aprovado")
+  } else if (nota.entre(4, 6.99)) {
+    console.log("recuperação")
+  } else if (nota.entre(0, 3.99)) {
+    console.log("Reprovado")
+  } else {
+    console.log("Nota inválida")
+  }
+  console.log("Fim")
 }
 
 imprimirResultado(10)
@@ -25,4 +23,3 @@ imprimirResultado(6.55)
 imprimirResultado(2.3)
 imprimirResultado(-1)
 imprimirResultado(11)
-
